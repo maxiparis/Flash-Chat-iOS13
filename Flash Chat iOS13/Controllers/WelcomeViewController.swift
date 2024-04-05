ded920791db65e0ba2452a87022d9fff0ca7b45f
“Max <mparis@hotmail.cl>
2024-04-05 00:28:05 -0600

    root view animation

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -11,12 +11,17 @@ import UIKit
 class WelcomeViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
-    
+    var titleText = "⚡️FlashChat"
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        titleLabel.text = ""
+        var count = 0.0
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * count, repeats: false) { timer in
+                self.titleLabel.text?.append(letter)
+            }
+            count += 1
+        }
+        
     }
-    
-
 }
